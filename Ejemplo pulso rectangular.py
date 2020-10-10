24c4525fe563ce8aa77139538afbc2926d3880e4
CorradoDeLuca <corrado.deluca@bubblebpm.com>
2020-10-10 16:38:31 -0300

    Creates new example

---
+++ Ejemplo pulso rectangular.py
@@ -0,0 +1,12 @@
+from F2Depine import soga
+
+def F(x):
+    if x <= 10/3:
+        return 0
+    elif x>= 10/3 and x <= 20/3:
+        return 3
+    else:
+        return 0
+
+pulso_rectangular = soga.CondicionesIniciales("cerrado", 10, 60, F, range(100))
+anim = pulso_rectangular.animate(1000)
